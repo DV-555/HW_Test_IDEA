73c8582283d7e4f4f299fc7171d3e97630fb39e7
valen <valentin.dumler@gmail.com>
2023-04-28 16:07:27 +0200

    create Enum Season

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,62 @@
-public class Main {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
-  public static void main(String[] args) {
-    System.out.println("Hello Valentin!");
+public class Main {
+  public enum Month {
+    JANUARY, // 0
+    FEBRUARY,
+    MARCH,
+    APRIL,
+    MAY,
+    JUNE,
+    JULY,
+    AUGUST,
+    SEPTEMBER,
+    OCTOBER,
+    NOVEMBER,
+    DECEMBER,
+  }
+
+   enum Season {
+    WINTER,
+    SPRING,
+    SUMMER,
+    AUTUMN,
+  }
+public static void main(String[] args) {
+    // в программе созданный enum можно использовать, как новый класс - как новый тип переменной
+    // (как int, String и т.д.)
+    Map<Month, Integer> daysPerMonth = new HashMap<>();
+    daysPerMonth.put(Month.JANUARY, 31);
+    daysPerMonth.put(Month.FEBRUARY, 28); // год не високосный
+    daysPerMonth.put(Month.MARCH, 31);
+    daysPerMonth.put(Month.APRIL, 30);
+    daysPerMonth.put(Month.MAY, 31);
+    daysPerMonth.put(Month.JUNE, 30);
+    daysPerMonth.put(Month.JULY, 31);
+    daysPerMonth.put(Month.AUGUST, 31);
+    daysPerMonth.put(Month.SEPTEMBER, 30);
+    daysPerMonth.put(Month.OCTOBER, 31);
+    daysPerMonth.put(Month.NOVEMBER, 30);
+    daysPerMonth.put(Month.DECEMBER, 31);
+
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Enter the month: ");
+    String month = scanner.next(); // месяц "как ввели"
+    scanner.close();
+    // enum.valueOf("ЗНАЧЕНИЕ") либо вернёт нам соответствующее значение по его названию,
+    // либо выбросит исключение IllegalArgumentException
+    Month monthKey = Month.valueOf(month.toUpperCase()); // месяц "как ключ словаря"
+    // enum.ordinal() возвращает индекс значения в перечислении
+    System.out.println(month + " has " + daysPerMonth.get(monthKey) + " days");
+
+    //May
+    //May has 31 days
+    //Month May is a Season SPRING
+
   }
 }
+
+
+
